924571476698e5e89b6853dcf1e4a74d8e544988
Robert Hitchcock <robert.e.hitchcock@gmail.com>
2023-03-28 15:26:48 -0400

    starting while loop

--- hangman.py
+++ hangman.py
@@ -6,7 +6,37 @@ def get_word():
     word = random.choice(word_list)
     return word.upper()
 
-   
+def play(word):
+      word_completion = "_" + len(word)
+      guessed = False
+      guessed_letters = []
+      guessed_words = []
+      tries = 6
+      print("Let's play CLI Hangman!")
+      print(display_hangman(tries))
+      print(word_completion)
+      print("\n")
+      while not guessed and tries > 0:
+         guess = input("Guess a letter or word:").upper()
+         if len(guess) == 1 and guess.isalpha():
+            if guess in guessed_letters:
+                print("You already guessed the letter", guess)
+            elif guess not in word:
+                print(guess, "is not in the word.")
+                tries -= 1
+                guessed_letters.append(guess)
+            else:
+                print("Yas Queen!", guess, "is in the word.")        
+         elif len(guess) == len(word) and guess.isalpha():
+
+         else: 
+             print("Not a valid guess.") 
+         print(display_hangman(tries))
+         print(word_completion)
+         print("\n")            
+
+
+
    
 
 def display_hangman(tries):
